20228d4907d0364956fcddb867574f23d9e0011b
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-01-22 12:07:07 +0800

    create linux dir

--- postinstall.js
+++ postinstall.js
@@ -4,5 +4,5 @@ var cp = require('child_process');
 var os = require('os');
 
 if (os.type() === 'Linux') {
-  cp.exec('cd pngdefry\/source && make && mv pngdefry ..\/bin\/linux', {});
+  cp.exec('mkdir -p pngdefry\/bin\/linux && cd pngdefry\/source && make &&  && mv pngdefry ..\/bin\/linux', {});
 }
