9061a87095e423616b3a07c83490f7b6db2c4549
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-01-22 13:27:16 +0800

    improve postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -2,7 +2,37 @@
 
 var cp = require('child_process');
 var os = require('os');
+var path = require('path');
+var fs = require('fs');
 
 if (os.type() === 'Linux') {
-  cp.exec('mkdir -p pngdefry\/bin\/linux && cd pngdefry\/source && make &&  && mv pngdefry ..\/bin\/linux', {});
+  var linuxPath = path.join(process.cwd(), 'pngdefry', 'bin', 'linux');
+  var pngdefryPath = path.join(process.cwd(), 'pngdefry', 'source', 'pngdefry');
+
+  if (!fsExistsSync(linuxPath)) {
+    fs.mkdir(linuxPath);
+  }
+
+  var makeFileDir = path.join(process.cwd(), 'pngdefry', 'source');
+
+  var make = cp.exec('cd ' + makeFileDir + ' && make', {});
+  make.on('exit', function(code) {
+    if (code !== 0) {
+      throw new Error('install fail');
+    }
+
+    cp.exec('mv ' + pngdefryPath + ' ' + linuxPath, {});
+  });
+
+}
+
+// ///////////////////
+function fsExistsSync(path) {
+  try {
+    fs.accessSync(path, fs.F_OK);
+  } catch (e) {
+    return false;
+  }
+
+  return true;
 }
