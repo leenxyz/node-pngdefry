a6ba52e37282de1aa8eb2d95e94855900b27fb46
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-01-26 15:38:35 +0800

    change mocha default timeout

--- test/index.js
+++ test/index.js
@@ -12,6 +12,7 @@ describe('Index', function() {
 
   describe('#pngdefry()', function() {
     it('should repair png success', function(done) {
+      this.timeout(5000);
       var input = path.join(__dirname, 'img', 'icon.png');
       var output = path.join(__dirname, 'img', 'icon-new.png');
 
